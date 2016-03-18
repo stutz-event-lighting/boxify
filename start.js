1b3de47ee21b11d30f928800b3a7a5a0c92af82e
Patrik Stutz <patrik.stutz@gmail.com>
2016-03-18 22:21:12 +0100

    moved last parts of control-center out of boxify, reworked patch system, deleted old patches, starded using co

--- start.js
+++ start.js
@@ -2,4 +2,6 @@ var fs = require("fs");
 var Boxify = require("./lib/index.js");
 
 var boxify = new Boxify(JSON.parse(fs.readFileSync("./config.json")+""));
-boxify.start();
+boxify.start().catch(function(err){
+    console.log(err.message,err.stack);
+});
