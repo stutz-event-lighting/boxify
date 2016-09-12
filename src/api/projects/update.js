056cec8c7a35e19bd6cd1d170302855858faea63
Patrik Stutz <patrik.stutz@gmail.com>
2016-09-12 12:58:00 +0200

    fixed bug with project comments not getting saved

--- src/api/projects/update.js
+++ src/api/projects/update.js
@@ -2,6 +2,6 @@ var mongo = require("mongodb");
 var parse = require("co-body");
 module.exports = function*(){
     var body = yield parse.json(this);
-    this.app.db.Project.update({_id:mongo.ObjectID(this.params.id)},{$set:{name:body.name,start:body.start,end:body.end,remark:body.remark}});
+    yield this.app.db.Project.update({_id:mongo.ObjectID(this.params.id)},{$set:{name:body.name,start:body.start,end:body.end,remark:body.remark}});
     this.status = 200;
 }
