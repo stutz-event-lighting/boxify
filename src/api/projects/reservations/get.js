baae0df61bdb6d7c4e7866171dbf9577b4d02b1c
Patrik Stutz <patrik.stutz@gmail.com>
2016-09-26 18:05:57 +0200

    added by-category layout to reservations

--- src/api/projects/reservations/get.js
+++ src/api/projects/reservations/get.js
@@ -1,15 +1,7 @@
 var mongo = require("mongodb");
 var util = require("../../../util");
 module.exports = async function(ctx){
-    var results = await Promise.all([
-        ctx.app.db.EquipmentReservation.findOne({_id:mongo.ObjectID(ctx.params.reservation)}).select("items"),
-        ctx.app.db.EquipmentType.find({}).select("name category"),
-        ctx.app.db.EquipmentCategory.find({}).select("name")
-    ])
+    var results = await ctx.app.db.EquipmentReservation.findOne({_id:mongo.ObjectID(ctx.params.reservation)}).select("items")
     ctx.set("Content-Type","application/json");
-    ctx.body = JSON.stringify({
-        reservation:results[0],
-        types:util.createIndex(results[1],"_id"),
-        categories:util.createIndex(results[2],"_id")
-    });
+    ctx.body = JSON.stringify(results);
 }
