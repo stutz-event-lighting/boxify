06b25394315b1ee0f1af9061bd4d65b71eb4e560
Patrik Stutz <patrik.stutz@gmail.com>
2016-10-04 19:36:27 +0200

    implemented document deleting, implemented delete button component

--- src/api/documents/delete.js
+++ src/api/documents/delete.js
@@ -1,3 +1,12 @@
+var mongo = require("mongodb");
 module.exports = async function(ctx){
-
+	var store = new mongo.GridStore(ctx.app.db.db,mongo.ObjectID(ctx.params.document),"w",{root:"documents"});
+    try{
+        await store.open();
+		await store.unlink();
+        await store.close();
+		ctx.status = 200;
+    }catch(e){
+        ctx.throw(404);
+    }
 }
