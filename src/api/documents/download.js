3e733a363b810be8e11b0b644b5b60c71a4f37da
Patrik Stutz <patrik.stutz@gmail.com>
2016-11-22 19:18:23 +0100

    display pdf & image documents directly in the browser

--- src/api/documents/download.js
+++ src/api/documents/download.js
@@ -6,7 +6,9 @@ module.exports = async function(ctx){
         await store.open();
 		ctx.body = await store.read();
 		ctx.set("Content-Type",store.contentType);
-        ctx.set("Content-Disposition","attachment; filename=\""+store.filename+"\"");
+        if(store.contentType != "application/pdf" && store.contentType.indexOf("image/") !== 0){
+            ctx.set("Content-Disposition","attachment; filename=\""+store.filename+"\"");
+        }
         await store.close();
     }catch(e){
         ctx.throw(404);
